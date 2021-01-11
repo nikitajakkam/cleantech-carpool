96a3afa03faaf5a9848eca7a93e7ccec739380cf
bucleantech2 <74393904+bucleantech2@users.noreply.github.com>
2021-01-11 12:13:10 -0500

    Changed homepage.html to the right homepage_cleantech.html

--- app.py
+++ app.py
@@ -214,7 +214,7 @@ def reroutetouser():
 
 @app.route('/cleantech/')
 def home(): #Home page ish kinda thing
-    return render_template('homepage.html')
+    return render_template('homepage_cleantech.html')
 
 
 @app.route('/cleantech/add_trip/')
