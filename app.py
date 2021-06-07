301655f87e54c9a3cc1cc4af000a57251381e478
Rahul Razdan <rahulrazdan09@example.com>
2021-06-07 11:14:11 -0400

    revert commit

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Created on Mon Sep  9 14:21:53 2019
 """
 
 #example from https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
-#testing
 import sqlite3
 import json
 import os #for supressing https warnings
